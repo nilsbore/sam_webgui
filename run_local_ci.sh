9165733928c4845e252a419f53194f6798772978
Nils Bore <nilsbore@gmail.com>
2020-10-19 16:48:44 +0200

    Got the basics of a CI running for this

---
+++ run_local_ci.sh
@@ -0,0 +1,1 @@
+rosrun industrial_ci run_ci ROS_DISTRO=melodic ROS_REPO=main DOCKER_RUN_OPTS="-e EMSDK -e EM_CONFIG -e EM_CACHE -e EMSDK_NODE -v ${EMSDK}:${EMSDK}" UPSTREAM_WORKSPACE="sam_webgui.rosinstall -sam_webgui"
