7a8e0a9932c54faf1d56afc153b88b03b626398d
Nils Bore <nbore@kth.se>
2021-01-19 23:00:56 +0100

    Updated to new version of roswasm

--- include/sam_webgui/roswasm_sam.h
+++ include/sam_webgui/roswasm_sam.h
@@ -8,24 +8,29 @@
 #include <nav_msgs/Odometry.h>
 
 #include <sam_msgs/PercentStamped.h>
-#include <sam_msgs/ThrusterRPMs.h>
+//#include <sam_msgs/ThrusterRPMs.h>
 #include <sam_msgs/ThrusterAngles.h>
 #include <sam_msgs/BallastAngles.h>
-#include <sam_msgs/Leak.h>
+//#include <sam_msgs/Leak.h>
+#include <smarc_msgs/Leak.h>
+#include <smarc_msgs/ThrusterRPM.h>
 
 namespace roswasm_webgui {
 
-bool draw_ballast_angles(sam_msgs::BallastAngles& msg, roswasm::Publisher* pub);
-bool draw_percent(sam_msgs::PercentStamped& msg, roswasm::Publisher* pub);
-bool draw_thruster_rpms(sam_msgs::ThrusterRPMs& msg, roswasm::Publisher* pub);
-bool draw_thruster_angles(sam_msgs::ThrusterAngles& msg, roswasm::Publisher* pub);
+bool draw_ballast_angles(sam_msgs::BallastAngles& msg, roswasm::Publisher& pub);
+bool draw_percent(sam_msgs::PercentStamped& msg, roswasm::Publisher& pub);
+//bool draw_thruster_rpms(sam_msgs::ThrusterRPMs& msg, roswasm::Publisher& pub);
+bool draw_thruster_rpm(smarc_msgs::ThrusterRPM& msg, roswasm::Publisher& pub);
+bool draw_thruster_angles(sam_msgs::ThrusterAngles& msg, roswasm::Publisher& pub);
 
 class SamActuatorWidget {
 private:
     TopicWidget<sam_msgs::ThrusterAngles>* thruster_angles;
-    TopicWidget<sam_msgs::ThrusterRPMs>* thruster_rpms;
-    roswasm::Publisher* rpm_pub;
-    roswasm::Timer* pub_timer;
+    TopicWidget<smarc_msgs::ThrusterRPM>* thruster1_rpm;
+    TopicWidget<smarc_msgs::ThrusterRPM>* thruster2_rpm;
+    roswasm::Publisher rpm1_pub;
+    roswasm::Publisher rpm2_pub;
+    roswasm::Timer pub_timer;
     bool rpm_pub_enabled;
     TopicWidget<sam_msgs::PercentStamped>* lcg_actuator;
     TopicWidget<std_msgs::Bool>* lcg_control_enable;
@@ -40,19 +45,21 @@ private:
 public:
     void pub_callback(const ros::TimerEvent& e);
     void show_window(bool& show_actuator_window);
-    SamActuatorWidget(roswasm::NodeHandle* nh);
+    SamActuatorWidget(roswasm::NodeHandle& nh);
 };
 
 class SamDashboardWidget {
 private:
     bool was_leak;
-    TopicBuffer<sam_msgs::Leak>* leak;
+    TopicBuffer<smarc_msgs::Leak>* leak;
     TopicBuffer<sensor_msgs::NavSatFix>* gps;
     TopicBuffer<sensor_msgs::BatteryState>* battery;
     TopicBuffer<nav_msgs::Odometry>* odom;
     TopicBuffer<sam_msgs::PercentStamped>* vbs;
     TopicBuffer<sam_msgs::PercentStamped>* lcg;
-    TopicBuffer<sam_msgs::ThrusterRPMs>* rpms;
+    //TopicBuffer<sam_msgs::ThrusterRPMs>* rpms;
+    TopicBuffer<smarc_msgs::ThrusterRPM>* rpm1;
+    TopicBuffer<smarc_msgs::ThrusterRPM>* rpm2;
     TopicBuffer<std_msgs::Float64>* depth;
     TopicBuffer<std_msgs::Float64>* pitch;
     TopicBuffer<std_msgs::Float64>* roll;
@@ -60,21 +67,23 @@ private:
 public:
     bool is_emergency() { return was_leak; }
     void show_window(bool& show_dashboard_window);
-    SamDashboardWidget(roswasm::NodeHandle* nh);
+    SamDashboardWidget(roswasm::NodeHandle& nh);
 };
 
 class SamTeleopWidget {
 private:
     bool enabled;
     sam_msgs::ThrusterAngles angles_msg;
-    sam_msgs::ThrusterRPMs rpm_msg;
-    roswasm::Publisher* rpm_pub;
-    roswasm::Publisher* angle_pub;
-    roswasm::Timer* pub_timer;
+    smarc_msgs::ThrusterRPM rpm1_msg;
+    smarc_msgs::ThrusterRPM rpm2_msg;
+    roswasm::Publisher rpm1_pub;
+    roswasm::Publisher rpm2_pub;
+    roswasm::Publisher angle_pub;
+    roswasm::Timer pub_timer;
 public:
     void pub_callback(const ros::TimerEvent& e);
     void show_window(bool& show_teleop_window);
-    SamTeleopWidget(roswasm::NodeHandle* nh);
+    SamTeleopWidget(roswasm::NodeHandle& nh);
 };
 
 } // namespace roswasm_webgui
